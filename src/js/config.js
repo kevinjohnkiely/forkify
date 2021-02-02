abcce453a9047a0c6e7aaccdcdc9a9ed32812fd9
kevinjohnkiely <kevinjohnkiely1@hotmail.com>
2021-02-02 15:33:27 +0000

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '194f44a6-8c90-4791-9abc-5beea5c6d37c';
+export const MODAL_CLOSE_SEC = 2.5;
